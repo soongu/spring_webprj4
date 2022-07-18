e4b0c76eec88f96096cdaf28535f7a574c13cb78
soongu <hsg9984@gmail.com>
2022-07-18 11:25:37 +0900

    [22.07.18] Board 도메인 클래스 추가

---
+++ src/main/java/com/project/web_prj/board/domain/Board.java
@@ -0,0 +1,17 @@
+package com.project.web_prj.board.domain;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Setter @Getter @ToString @EqualsAndHashCode
+@NoArgsConstructor @AllArgsConstructor
+public class Board {
+
+    private Long boardNo;
+    private String writer;
+    private String title;
+    private String content;
+    private Long viewCnt;
+    private Date regDate;
+}
