2a3df4b3a2b0d4fbdcefe40f3e7d8ccacc28c327
soongu <naong526@naver.com>
2022-09-02 17:32:45 +0900

    [22.09.02] 배포완료

--- src/main/java/com/project/web_prj/config/DataBaseConfig.java
+++ src/main/java/com/project/web_prj/config/DataBaseConfig.java
@@ -2,16 +2,30 @@ package com.project.web_prj.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 // DB 관련 설정 클래스
 @Configuration
 @ComponentScan(basePackages = "com.project.web_prj")
+@PropertySource("classpath:db_info.properties")
 public class DataBaseConfig {
+
+    @Value("${aws.rds_user_name}")
+    private String username;
+
+    @Value("${aws.rds_password}")
+    private String password;
+
+    @Value("${aws.rds_url}")
+    private String url;
+
+
 
     // DB접속 정보 설정 (DataSource설정)
     @Bean
@@ -19,9 +33,9 @@ public class DataBaseConfig {
 
         HikariConfig config = new HikariConfig();
 
-        config.setUsername("goguma");
-        config.setPassword("hsg88490!");
-        config.setJdbcUrl("jdbc:mariadb://goguma-dev-database.cbcszxd0fody.ap-northeast-2.rds.amazonaws.com:3306/spring4");
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setJdbcUrl(url);
         config.setDriverClassName("org.mariadb.jdbc.Driver");
 
         return new HikariDataSource(config);
